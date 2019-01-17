fc090ce163558e9b20b4906cd7295828d8af36c7
vymik <v.mikalonis@gmail.com>
2019-01-17 22:35:32 +0200

    fixed reccuring events view, cleaned code

--- routes/calendar.js
+++ routes/calendar.js
@@ -1,95 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var path = require('path');
+var path = require("path");
 var bodyParser = require("body-parser");
-const Workoutevent = require('../models/Workoutevent')
+const Workoutevent = require("../models/Workoutevent");
+var calController = require("../controllers/CalendarController");
 
-// var db = require('mongoskin').db("mongodb://localhost/testdb", { w: 0});
-// 	db.bind('event');
-
-
-router.get('/', function(req, res, next) {
-    // console.log("veikia>>>>>>>>>>>>>>>>>>>");
-    res.render('calendar/calendar');
-  });
-
-router.get('/data', function(req, res){
-    // console.log("veikia>>>>>>>>>>>>>>>>>>>>");
-	Workoutevent.find({}, function(err, data){
-        console.log(data)
-		//set id property for all records
-		for (var i = 0; i < data.length; i++)
-			data[i].id = data[i]._id;
-		
-		//output response
-		res.send(data);
-	});
-});
-
-
-router.post('/data', function(req, res){
-    console.log(req.body);
-	var data = req.body;
-	var mode = data["!nativeeditor_status"];
-	var sid = data.id;
-	var tid = sid;
-
-	delete data.id;
-	delete data.gr_id;
-	delete data["!nativeeditor_status"];
-
-
-	function update_response(err, result){
-		if (err) {
-			mode = "error";
-        } else if (mode == "inserted") {
-			tid = data._id;
-        }    
-		res.setHeader("Content-Type","application/json");
-		res.send({action: mode, sid: sid, tid: tid});
-	}
-
-	if (mode == "updated") {
-        // console.log("updatinam>>>>>>>>>>>>>>>");
-    Workoutevent.findById(sid, (err, eventFromDB)=>{
-        eventFromDB.text = data.type;
-        eventFromDB.start_date = data.start_date;
-        eventFromDB.end_date = data.end_date;
-        eventFromDB.event_pid = data.event_pid;
-        eventFromDB.event_length = data.event_length;
-        eventFromDB.rec_type = data.rec_type;
-        
-        eventFromDB.save(function(err, post) {
-        if (err) throw err;
-            res.redirect('/calendar');
-        });  
-    })
-    } else if (mode == "inserted") {
-        // console.log(" iki cia daejo >>>>>>");
-		let newEvent = new Workoutevent({
-            start_date: data.start_date,
-            end_date: data.end_date,
-            text: data.type,
-            event_pid: data.event_pid,
-            event_length: data.event_length,
-            rec_type: data.rec_type
-        });
-        newEvent.save();
-    } else if (mode == "deleted") {
-        // console.log('trinam>>>>>>>>>>>')
-		Workoutevent.findOneAndDelete({ "_id": sid  }, (error, data)=>{
-            if(error){
-                console.log("error in deleting!");
-                throw error;
-            } else {
-                console.log("data all gone and deleted");
-                res.status(204);
-            }
-        });
-    } else {
-        res.send("Not supported operation");
-    }    
-    // res.redirect('/calendar');
-});
+router.get("/", calController.calendarHome);
+router.get("/data", calController.loadEvents);
+router.post("/data", calController.doEvent);
 
 module.exports = router;
