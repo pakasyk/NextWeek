f06b31b615b0bcc651f1bea6b68e422fc18fd6e4
Ugnius Vaiciulis <ugniusvaiciulis@Ugniuss-Air.lan>
2019-01-21 12:14:35 +0200

    pages added

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var LocalStrategy = require('passport-local').Strategy;
 var session = require('express-session');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var pagesRouter = require('./routes/pages');
 
 var app = express();
 
@@ -52,6 +53,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/', pagesRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
